82c32159bfbb5ac0e3de7a03bc53cdda22f228ea
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-03-13 21:19:47 +0630

    Added HTML Module 4

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -757,6 +757,22 @@ final Map<int, Lesson> htmlLessons = {
   13: Lesson(2, 13),
   14: Lesson(2, 14),
   15: Lesson(2, 15),
+  16: Lesson(3, 16),
+  17: Lesson(3, 17),
+  18: Lesson(3, 18),
+  19: Lesson(3, 19),
+  20: Lesson(3, 20),
+  21: Lesson(3, 21),
+  22: Lesson(3, 22),
+  23: Lesson(3, 23),
+  24: Lesson(3, 24),
+  25: Lesson(4, 25),
+  26: Lesson(4, 26),
+  27: Lesson(4, 27),
+  28: Lesson(4, 28),
+  29: Lesson(4, 29),
+  30: Lesson(4, 30),
+  31: Lesson(4, 31),
 };
 
 final Map<String, Map<int, Lesson>> lessons = {
