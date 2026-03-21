0b76da55ae8977d3b9c1be4465bc4c9615549fc5
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-03-21 09:39:33 +0630

    Added C Module 2

--- lib/languages/lang_c.dart
+++ lib/languages/lang_c.dart
@@ -47,4 +47,11 @@ final Map<int, Lesson> cLessons = {
   4: Lesson(1, 4),
   5: Lesson(1, 5),
   6: Lesson(1, 6),
+  7: Lesson(2, 7),
+  8: Lesson(2, 8),
+  9: Lesson(2, 9),
+  10: Lesson(2, 10),
+  11: Lesson(2, 11),
+  12: Lesson(2, 12),
+  13: Lesson(2, 13),
 };
